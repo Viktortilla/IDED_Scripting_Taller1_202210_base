405d5ef63832a1f3a0eba6d50c155df4823bf4a2
Viktortilla <victormanuelvtr62@gmail.com>
2022-02-18 18:36:29 -0500

    punto 2

--- TestMethods.cs
+++ TestMethods.cs
@@ -58,19 +58,89 @@ namespace TestProject1
 
         internal static Dictionary<int, EValueType> FillDictionaryFromSource(int[] sourceArr)
         {
-            Dictionary<int, EValueType> result = null;
+            Dictionary<int, EValueType> result = new Dictionary<int, EValueType>();
+            for(int i = 0; i <= sourceArr.Length-1; i++)
+            {
+                if (sourceArr[i] % 2 == 0)//el numero es par
+                {
+                    result.Add(sourceArr[i], EValueType.Two);
+                }
+                else if (sourceArr[i] % 3 == 0)
+                {
+                    result.Add(sourceArr[i], EValueType.Three);
+                }
+                else if (sourceArr[i] % 5 == 0)
+                {
+                    result.Add(sourceArr[i], EValueType.Five);
+                }
+                else if (sourceArr[i] % 7 == 0)
+                {
+                    result.Add(sourceArr[i], EValueType.Seven);
+                }
+                else if (sourceArr[i] % 5 == 0)
+                {
+                    result.Add(sourceArr[i], EValueType.Five);
+                }
+                else if (SourceArrAnalysis(sourceArr[i]))
+                {
+                    result.Add(sourceArr[i], EValueType.Prime);
+                }
+            }
+            
 
             return result;
+        }
+        internal static bool SourceArrAnalysis(int valor)//para saber si es primo
+        {
+            bool resultado = false;
+            int count = 0;
+            for(int i =1; i<= valor; i++)
+            {
+                if (valor % i == 0)
+                {
+                    count = count + 1;
+                }
+            }
+            if (count <= 2)
+            {
+                resultado = true;
+            }
+            return resultado;
         }
 
         internal static int CountDictionaryRegistriesWithValueType(Dictionary<int, EValueType> sourceDict, EValueType type)
         {
-            return 0;
+
+            int retorno = 0;
+            foreach(var value in sourceDict.Values)
+            {
+                if(value == type)
+                {
+                    retorno = retorno + 1;
+                }
+            }
+
+
+            return retorno;
         }
 
         internal static Dictionary<int, EValueType> SortDictionaryRegistries(Dictionary<int, EValueType> sourceDict)
         {
-            Dictionary<int, EValueType> result = null;
+            Dictionary<int, EValueType> result = new Dictionary<int, EValueType>();
+
+            List<int> l_keys = new List<int>();
+            
+
+            foreach (var key in sourceDict.Keys)
+            {
+                l_keys.Add(key);
+            }
+            l_keys.Sort();
+            l_keys.Reverse();
+
+            int[] A_keys = l_keys.ToArray();
+
+            result = FillDictionaryFromSource(A_keys);
 
             return result;
         }
